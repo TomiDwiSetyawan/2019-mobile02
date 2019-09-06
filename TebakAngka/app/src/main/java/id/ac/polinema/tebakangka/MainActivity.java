6f85400362deafc78b311cce10e62c1262a9c040
Tomi Dwi Setyawan <tomidwisetyawan1@gmail.com>
2019-09-06 16:51:02 +0700

    TUGAS TebakAngka

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,24 +13,51 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText number_input;
+	private int n, convert;
+	private Random rnd;
+	private Button guess_button;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		number_input = findViewById(R.id.number_input);
+		guess_button = findViewById(R.id.guess_button);
+//		number_input = findViewById(R.id.number_input);
+		rnd = new Random();
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-
+		n = rnd.nextInt(100)+1;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+
+		if(number_input.getText().toString().isEmpty()){
+			Toast.makeText(this, "Wajib Input Angka", Toast.LENGTH_SHORT).show();
+		}else {
+			String no = number_input.getText().toString();
+			convert = Integer.parseInt(no);
+			if (convert == n){
+				Toast.makeText(this, "Tebakan Anda Benar!", Toast.LENGTH_SHORT).show();
+				guess_button.setEnabled(false);
+			}else if(convert > n){
+				Toast.makeText(this, "Tebakan Anda Telalu Besar!", Toast.LENGTH_SHORT).show();
+			}else if(convert < n){
+				Toast.makeText(this, "Tebakan Anda Terlalu Kecil", Toast.LENGTH_SHORT).show();
+			}
+		}
 	}
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		number_input.setText("");
+		initRandomNumber();
+		guess_button.setEnabled(true);
 	}
 }
